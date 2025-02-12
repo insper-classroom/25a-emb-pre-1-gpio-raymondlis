a2bc89a2794f064fef15e28f9c8b23e202e3c434
raymondlis <raylisbona@gmail.com>
2025-02-11 23:13:36 -0300

    ex1,2

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,35 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int LED_PIN_R = 4;
+const int LED_PIN_G = 6;
+const int BTN_PIN_G = 26;
+const int BTN_PIN_R = 28;
+
 int main() {
   stdio_init_all();
+  
+  gpio_init(LED_PIN_R);
+  gpio_set_dir(LED_PIN_R, GPIO_OUT);
+  
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+  
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
 
   while (true) {
+    if (!gpio_get(BTN_PIN_G)) {
+      gpio_put(LED_PIN_G, 1);
+    }
+    
+    if (!gpio_get(BTN_PIN_R)) {
+      gpio_put(LED_PIN_R, 1);
+    }
   }
 }
